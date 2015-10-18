989f1b34351564a3f03d81d69ad743491abbf50f
Lucas <lucas@agtlucas.com>
2015-10-18 09:16:56 -0200

    Works?

--- Coal/ViewController.swift
+++ Coal/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "http://httpbin.org/get")
     }
 
     override func didReceiveMemoryWarning() {
